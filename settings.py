47b6e69651b09ca3f7a041b404fff157fd79a3db
gary_du <dyxlut@126.com>
2022-03-22 17:50:47 +0800

    Settings_version 1.

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+	"""store the settings in game Alien invasion"""
+
+	def __init__(self):
+		"""initiate the settings of the game"""
+		#screen settings
+		self.screen_width=1200
+		self.screen_height=800
+		self.bg_color=(230,230,230)
