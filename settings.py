665949f2c0132984d3237ca0a3bdae5b69a80b3f
gary_du <dyxlut@126.com>
2022-03-25 11:13:53 +0800

    Settings change in screen_width.

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ class Settings:
 	def __init__(self):
 		"""initiate the settings of the game"""
 		#screen settings
-		self.screen_width=1200
+		self.screen_width=1550
 		self.screen_height=800
 		self.bg_color=(230,230,230)
