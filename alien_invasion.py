dc432f50485543f705ddc923eb8b844b2cd83d57
gary_du <dyxlut@126.com>
2022-03-26 09:57:03 +0800

    rebuild _check_event(self).

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,4 +1,5 @@
-#rebuild use _check_events(),_update_screen()
+#rebuild _check_events_()
+#As this program is growing increasingly complex, the method: _check_event_()would be longer and longer, so rebuild is desperatedly needed.
 import pygame
 import sys
 from settings import Settings
@@ -10,7 +11,7 @@ class AlienInvasion:
 		"""initiate the game and build the assets"""
 		pygame.init()
 		self.settings=Settings()
-		self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height)) #surface_1:屏幕。 赋给self.screen的是一个surface，surface是屏幕的一部分，用于显示游戏元素，每个元素如（ship和alien）都是一个surface
+		self.screen=pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
 		self.ship=Ship(self)
 		pygame.display.set_caption("Alien Invasion")
 
@@ -28,25 +29,34 @@ class AlienInvasion:
 				sys.exit()
 				
 			elif event.type==pygame.KEYDOWN:
-				if event.key==pygame.K_RIGHT:
-					self.ship.moving_right=True
-				if event.key==pygame.K_LEFT:
-					self.ship.moving_left=True
-				if event.key==pygame.K_UP:
-					self.ship.moving_up=True
-				if event.key==pygame.K_DOWN:
-					self.ship.moving_down=True
+				self._check_keydown_events(event)
+				
 
 			elif event.type==pygame.KEYUP:
-				if event.key==pygame.K_RIGHT:
-					self.ship.moving_right=False
-				if event.key==pygame.K_LEFT:
-					self.ship.moving_left=False
-				if event.key==pygame.K_UP:
-					self.ship.moving_up=False
-				if event.key==pygame.K_DOWN:
-					self.ship.moving_down=False
+				self._check_keyup_events(event)
 					
+	def _check_keydown_events(self,event):
+		"""respond to the keydown events"""
+		if event.key==pygame.K_RIGHT:
+			self.ship.moving_right=True
+		if event.key==pygame.K_LEFT:
+			self.ship.moving_left=True
+		if event.key==pygame.K_UP:
+			self.ship.moving_up=True
+		if event.key==pygame.K_DOWN:
+			self.ship.moving_down=True
+
+	def _check_keyup_events(self,event):
+		"""respond to the keyup events"""
+		if event.key==pygame.K_RIGHT:
+			self.ship.moving_right=False
+		if event.key==pygame.K_LEFT:
+			self.ship.moving_left=False
+		if event.key==pygame.K_UP:
+			self.ship.moving_up=False
+		if event.key==pygame.K_DOWN:
+			self.ship.moving_down=False
+
 	def _update_screen(self):
 		"""update the images on the screen and switch to new screens"""
 		pygame.display.flip()
